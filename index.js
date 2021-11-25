0becbe7ade32de13c1993c56883e12f32ff20aac
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2021-11-25 13:23:17 +0200

    Update

--- index.js
+++ index.js
@@ -3,15 +3,35 @@ const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 const shopRoute = require("./routes/shop");
+const winston = require("winston");
 
 const PORT = process.env.PORT || 3000
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/shop", shopRoute);
+
+
+//create a logger
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize({ all: true })
+            )
+        }),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    ],
+    exceptionHandlers: [
+        new winston.transports.File({ filename: 'exceptions.log' })
+    ]
+});
 
 //routes
+app.use("/api/shop", shopRoute);
 
 
 //connect to mongodb atlas 
@@ -19,12 +39,12 @@ mongoose.connect(process.env.MONGO_URL,
     { useNewUrlParser: true }
 )
     .then(() => {
-        console.log("Connected to mongodb atlas")
+        logger.info("connected to mongoDb atlas");
     }).catch(error => {
-        console.log("Something wirng happened", error)
+        logger.error(error.mongoose);
     })
 
 //Start Server
 app.listen(PORT, () => {
-    console.log("Server started at PORT ", PORT);
+    logger.info(`Server started at PORT${PORT}`);
 })
--- routes/shop.js
+++ routes/shop.js
@@ -23,4 +23,45 @@ router.post("/", async (req, res) => {
         });
 });
 
+
+//GET ALL SHOP
+router.get("/", (req, res) => {
+    Shop.find()
+        .then((shops) => res.send(shops))
+        .catch((error) => {
+            res.status(500).send("Something went wrong")
+        });
+});
+
+
+//GET THE SHOP BY ID 
+router.get("/:bookId", async (req, res) => {
+    const book = await Shop.findById(req.params.bookId)
+    if (!book) res.status(404).send("Book not work");
+    res.send(book);
+});
+
+// Update Shop Base ON ID
+router.put("/:shopId", async (req, res) => {
+    const updateShop = await Shop.findByIdAndUpdate(
+        req.params.shopId, {
+        name: req.body.shopName,
+        author: {
+            name: req.body.authorName,
+            age: req.body.authorAge
+        },
+        genre: req.body.genre
+    }, { new: true })
+    if (!updateShop) res.status(404).send("shop nont found");
+    res.send(updateShop)
+});
+
+
+//DELETE SHOP Base ON ID
+router.delete("/:shopId", async (req, res) => {
+    const shop = await Shop.findByIdAndRemove(req.params.shopId);
+    if (!shop) res.status(404).send("shop with id not found");
+    res.send(shop);
+});
+
 module.exports = router;
